1e1df1f1c17fd12d82630d92d8b82f62a224f386
Reina Lizaso <reinaalizaso@gmail.com>
2025-01-22 16:00:13 -0700

    Cube now moves on x-axis and rotates on z-axis

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -6,10 +6,10 @@ public class TransformController : MonoBehaviour
     {
     //Move the target GameObject
     var x = Mathf.PingPong(Time.time, 3);
-    var p = new Vector3(0, x, 0);
+    var p = new Vector3(x, 0, 0);
     transform.position = p;
 
     //Rotate the target GameObject
-    transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+    transform.Rotate(new Vector3(0, 0, 50) * Time.deltaTime);
     }
 }
